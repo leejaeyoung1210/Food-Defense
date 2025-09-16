c1b7309463bc6a45bbd871627bbf22a57d5c25a0
leejaeyoung <kltop1210@gmail.com>
2025-09-16 14:11:15 +0900

    TowerDie

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -41,7 +41,7 @@ public class Enemy : MonoBehaviour
         targetposition = currentPath.GetWayPoint(currentPoint);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Tower"))
         {
